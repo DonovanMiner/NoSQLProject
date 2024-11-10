7622d2826059bb36d46d8c2035012099adc126fb
Donovan <dminer1@fordham.edu>
2024-11-10 13:02:52 -0500

    updates

--- NoSQLProject/settings.py
+++ NoSQLProject/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    
+    #dev'd apps
+    "Dashboard.apps.DashboardConfig",
 ]
 
 MIDDLEWARE = [
@@ -73,12 +76,15 @@ WSGI_APPLICATION = "NoSQLProject.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+#MongoDB references in root/utils.py
+#DATABASES = {
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
+    
+
+#}
 
 
 # Password validation
