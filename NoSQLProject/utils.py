0e8bf42c626926d9d44a12960fddbc518ab30712
Donovan <dminer1@fordham.edu>
2024-11-12 17:15:39 -0500

    changed db user

--- NoSQLProject/utils.py
+++ NoSQLProject/utils.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
 
-client = MongoClient('mongodb+srv://dminer:Aahp9NXCLPGO7Pdk@nosqltestcluster.5q339.mongodb.net/')
+client = MongoClient('mongodb+srv://test:Wl3Y3BsOs5FEbXa0@nosqltestcluster.5q339.mongodb.net/')
 db = client['NoSQL_ProjectDatabase']
 user_fitness_data = db['user_fitness_data']
