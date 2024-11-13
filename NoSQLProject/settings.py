e7801d50f52fd24cba52b39feb1326afd6fc0ef0
Andres De Los Santos <delossantosa574@gmail.com>
2024-11-13 16:57:21 -0500

    trying to link different tabs on the webapp

--- NoSQLProject/settings.py
+++ NoSQLProject/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = "NoSQLProject.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],  # Global template directory
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -122,7 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
