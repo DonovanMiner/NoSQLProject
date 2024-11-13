4baf53296151047a1b38e4e752c03190474b6c94
Andres De Los Santos <157183643+andresdls24@users.noreply.github.com>
2024-11-13 13:39:21 -0500

    Update views.py
    deleted the HttpResponse

--- Landing/views.py
+++ Landing/views.py
@@ -8,27 +8,27 @@ from NoSQLProject.utils import user_fitness_data #import clinet, db as well?
 def home(request):
     res = user_fitness_data.find().limit(10)
     context = {'res' : res}
-    return HttpResponse(render(request, 'Landing/home.html', context))
+    return render(request, 'Landing/home.html', context)
 
 # workouts page view
 def workouts(request):
     workout_data = user_fitness_data.find().limit(10)
     context = {'workout_data': workout_data}
-    return HttpResponse(render(request, 'landing/workouts.html', context)
+    return render(request, 'landing/workouts.html', context)
 
 # how it works page view
 def how_it_works(request):
     context = {} #going to be defined later since it will not use data
-    return HttpResponse(render(request, 'landing/how_it_works.html', context)
+    return render(request, 'landing/how_it_works.html', context)
 
 # login/sign-up page view
 def login_signup(request):
     context = {} #this will be empty until we find out how to authenticate users and stuff.
-    return HttpResponse(render(request, 'landing/login_signup.html', context)
+    return render(request, 'landing/login_signup.html', context)
 
 ## After login request (dashboard view)
 def dashboard(request):
     user_name = request.user.first_name
     progress = {"completed_goals": 3, "total_goals": 5}
     context = {"user_name": user_name, "progress": progress}
-    return HttpResponse(render(request, 'landing/dashboard.html', context)
+    return render(request, 'landing/dashboard.html', context)
