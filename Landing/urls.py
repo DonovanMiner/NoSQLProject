5ffd1f88f7bac9c0114903389626e25985f1470a
Andres De Los Santos <157183643+andresdls24@users.noreply.github.com>
2024-11-13 13:23:58 -0500

    Update urls.py
    update urls

--- Landing/urls.py
+++ Landing/urls.py
@@ -4,6 +4,9 @@ from . import views
 app_name = 'Landing'
 
 url_patterns = [
-    
-    
+    path('', views.home, name='home'), 
+    path('workouts/', views.workouts, name = 'workouts'),
+    path('how-it-works/', views.how_it_works, name = 'how_it_works'),
+    path('login-signup/', views.login_signup, name = 'login_signup'),
+    path('dashboard/', views.dashboard, name = 'dashboard'),  
     ]
