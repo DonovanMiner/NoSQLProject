61c0662b13cc385a287795c6328de06e43cecc0d
Donovan <dminer1@fordham.edu>
2024-11-16 18:01:30 -0500

    added graph to user dash

--- Dashboard/views.py
+++ Dashboard/views.py
@@ -1,8 +1,15 @@
+from cgitb import text
+from turtle import title
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
+from numpy import kaiser
+from plotly.graph_objs import XAxis
 
-from NoSQLProject.utils import user_fitness_data
+from NoSQLProject.utils import user_fitness_data, users
+
+import plotly.graph_objects as go
+import pandas as pd
 
 def default_dashboard(request):
     
@@ -19,12 +26,28 @@ def user_dashboard(request):
 
     u_name = request.POST.get('username')
     password = request.POST.get('password')
-    print(f'UNAME CHECK: {type(u_name)} {u_name}')
-    print(f'PASSWORD CHECK: {type(password)} {password}')
+    #print(f'UNAME CHECK: {type(u_name)} {u_name}')
+    #print(f'PASSWORD CHECK: {type(password)} {password}')
 
+    u_id = users.find_one({"u_name" : u_name, "password" : password}, {"_id" : 0, "user_id" : 1})
+    #print(f'U_ID CHECK: {type(u_id)} {u_id}')
+    
+    #starting here, process to be put into a function call to create graphs
+    df_1 = user_fitness_data.find({"user_id" : u_id['user_id'], "workout_type" : "Cycling"}).sort({"date" : -1})
+    df_1 = [(doc['date'], doc['distance_km'], doc['active_minutes']) for doc in df_1] 
+    df_1 = pd.DataFrame(df_1)
+    df_1[1] = df_1[1] / (df_1[2]/60)
+    
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=df_1[0], y=df_1[1]))
+    fig.update_layout(title=f'User {u_name} Cycling')
+    fig.update_xaxes(title='Date')
+    fig.update_yaxes(title='Km/Hr')
+    fig = fig.to_html()
+    #ending here
+    
+    #print(f'DATA QUERY CHECK: {type(df_1)} {df_1}')
 
-    u_id = user_fitness_data.find({"u_name" : u_name, "password" : password})
-    context = {"u_id" : u_id}
-    print(f'U_ID CHECK: {type(u_id)} {u_id}')
+    context = {"u_id" : u_id, "fig" : fig}
     
     return HttpResponse(render(request, 'Dashboard/user_dashboard.html', context))
