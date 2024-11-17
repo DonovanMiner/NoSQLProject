a1afcc6eab3394a633e9340e421da92726d4f130
Andres De Los Santos <delossantosa574@gmail.com>
2024-11-17 14:47:21 -0500

    Improve of login template and redirection
    added 2 forms for login/signup tab, so users can create or log into their accounts.
    
    deleted lines related to workout page
    
    redirect user to dashboard
    
    need to fix user auth with mongodb

--- Landing/urls.py
+++ Landing/urls.py
@@ -5,8 +5,6 @@ app_name = 'Landing'
 
 urlpatterns = [
     path('', views.home, name='home'), 
-    path('workouts/', views.workouts, name='workouts'),
     path('how-it-works/', views.how_it_works, name='how_it_works'),
     path('login-signup/', views.login_signup, name='login_signup'),
-    path('dashboard/', views.dashboard, name='dashboard'),  
 ]
