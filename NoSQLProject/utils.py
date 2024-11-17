e6c6bcc775e2e6a659ca65041eea4ee866245f5a
Andres De Los Santos <delossantosa574@gmail.com>
2024-11-17 18:58:44 -0500

    Update utils.py in NoSQL
    Copied the same code as in the main branch

--- NoSQLProject/utils.py
+++ NoSQLProject/utils.py
@@ -3,4 +3,4 @@ from pymongo import MongoClient
 client = MongoClient('mongodb+srv://test:Wl3Y3BsOs5FEbXa0@nosqltestcluster.5q339.mongodb.net/')
 db = client['NoSQL_ProjectDatabase']
 user_fitness_data = db['user_fitness_data']
-users = db['users'] 
+users = db['users']
