d20072f5e2a5953c78942b97649f391743c3cbd8
Andres De Los Santos <delossantosa574@gmail.com>
2024-11-20 23:57:13 -0500

    Updated login-signup form
    Created a new users form for login, that asks a couple of questions from a new client, such as name, last name, email, etc. Additionally, added some dropdowns list for height and weight.
    
    Password field should follow an alphanumeric charcaters between 8 and 12.
    
    updated users collection in mongodb adding the required information for users.
    
    Need to fix the height dropdown menu and check the dashboard direction, so it retrieves correctly

--- Landing/views.py
+++ Landing/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
-from django.contrib.auth import authenticate, login #ask Donovan if he could fix the user authentication problem with the database in MongoDB
-from django.contrib.auth.models import User
-from django.contrib import messages 
+from datetime import datetime
+from django.contrib.auth.hashers import make_password, check_password
 
-
-from NoSQLProject.utils import user_fitness_data #import clinet, db as well?
+from NoSQLProject.utils import user_fitness_data, users, goals #import clinet, db as well?
 
 # Home page view (before login)
 def home(request):
@@ -16,53 +14,99 @@ def home(request):
 
 # how it works page view
 def how_it_works(request):
-    context = {} #this will be defined later since it will not use data
+    context = {} #this will be updated later since it will not use data
     return render(request, 'Landing/how_it_works.html', context)
 
-# login/sign-up page view
+# Login/Sign-up page view
 def login_signup(request):
+    # Define the height drop down list options range from 4'0" to 7'11"
+    heights = []
+    for ft in range(4, 8):  
+        for in_ in range(0, 12): 
+            heights.append(f"{ft}' {in_:02d}\"")  
+
     if request.method == "POST":
-        action = request.POST.get('action')  #this is for login or sigunp
+        action = request.POST.get('action')
         
-        #Handling login:
+        # Handling login:
         if action == 'login':
             username = request.POST['username']
             password = request.POST['password']
+
+            # Find the user in the MongoDB collection based on the u_name (username)
+            user = users.find_one({'u_name': username})
             
-            user = authenticate(request, username=username, password=password)
-            
-            if user is not None:
-                login(request, user)
-                return redirect('Dashboard:user_dashboard') # Redirect to dashboard after login
+            if user and check_password(password, user['password']):
+                request.session['user_id'] = user['user_id']
+                return redirect('Dashboard:user_dashboard') 
             else:
                 messages.error(request, "Invalid username or password")
                 return redirect('Landing:login_signup')
         
-        #Handling sign-up
+        # Handling sign-up:
         elif action == 'signup':
-            new_username = request.POST['new_username']
-            new_password = request.POST['new_password']
+            u_name = request.POST['u_name']
+            f_name = request.POST['f_name']
+            l_name = request.POST['l_name']
+            email_addr = request.POST['email_addr']
+            password = request.POST['password']
+            gender = request.POST['gender']
+            dob = request.POST['dob']
+            height = request.POST['height']  
+            weight = request.POST['weight']
             
-            #Check if username exists in the db:
-            if User.objects.filter(username=new_username).exists():
-                messages.error(request, "Username already taken")
+            # Find and assign the next available number for as the user_id for this new_user
+            last_user = users.find().sort("user_id", -1).limit(1)
+            new_user_id = last_user[0]['user_id'] + 1 if last_user.count() > 0 else 1
+            
+            # capturing the account creation date
+            date_of_creation = datetime.now()
+
+            # Checking if email already exists in the MongoDB 'users' collection
+            existing_user = users.find_one({'email_addr': email_addr})
+            if existing_user:
+                messages.error(request, "Email already in use")
                 return redirect('Landing:login_signup')
             
-            #create a new user:
-            user = User.objects.create_user(username=new_username, password=new_password)
-            user.save()
+            #hash the password before saving it - i do not think its necessary, but the best practices do it for production environment
+            hashed_password = make_password(password)
+
+            # Create a new user document in MongoDB:
+            new_user_data = {
+                'user_id': new_user_id,
+                'u_name': u_name,
+                'f_name': f_name,
+                'l_name': l_name,
+                'email_addr': email_addr,
+                'password': hashed_password,  #Should we consider hashing the passwords??
+                'gender': gender,
+                'dob': dob,
+                'height': height,
+                'weight': weight,
+                'date_of_creation': date_of_creation, ##shoudl we save the time of creation??
+            }
+
+            # Store the new_user_data into the users collection in mongodb
+            users.insert_one(new_user_data)
             
-            #redirect new user to dashboard mainpage (log immediately)
-            login(request, user)
+            # Log the new user in automatically
+            request.session['user_id'] = new_user_data['user_id']
+
             return redirect('Dashboard:user_dashboard')
     
-    return render(request, 'Landing/login_signup.html')
+    return render(request, 'Landing/login_signup.html', {'heights': heights})
 
-#After login request (dashboard view)
-# Needs to be configured with the actual attributes of the database related to the users collection.
+##### check this
 def dashboard(request):
-    user_name = request.user.first_name
-    user_progress = user_fitness_data.find_one({'user_id':request.user.id}) #adjust query according to our actual database.
-    progress = user_progress.get('progress', {'completed_goals': 0, 'total_goals': 0})
-    context = {'user_name':user_name, 'progress': progress}  #Edit when we find out how to authenticate users and stuff.
+    user_id = request.session.get('user_id') 
+    user = users.find_one({'user_id': user_id})
+
+    if user:
+        user_name = user['f_name']
+        user_progress = user_fitness_data.find_one({'user_id': user_id}) 
+        progress = user_progress.get('progress', {'completed_goals': 0, 'total_goals': 0}) ### check this
+        context = {'user_name': user_name, 'progress': progress}
+        return render(request, 'Landing/dashboard.html', context)
+    else:
+        return redirect('Landing:login_signup')  # If no user found, redirect to login
     return render(request, 'Landing/dashboard.html', context)
