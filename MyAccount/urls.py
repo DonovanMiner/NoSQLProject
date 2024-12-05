9b0d603f4d9fd0e6ed01c5e6134b004bcbd6159a
Andres De Los Santos <delossantosa574@gmail.com>
2024-12-04 20:41:49 -0500

    modified account app
    still working with the linking so i can perfomr CRUD operations

--- MyAccount/urls.py
+++ MyAccount/urls.py
@@ -1,16 +1,10 @@
 
 from django.urls import path
-from . import views  
+from . import views
 
-app_name = 'MyAccount'  
+app_name = 'MyAccount'
 
 urlpatterns = [
-    path('', views.profile_settings, name='profile_settings'),
-    path('change-profile-photo/', views.change_profile_photo, name='change_profile_photo'),
-    path('change-password/', views.change_password, name='change_password'),
-    path('change-bio/', views.change_bio, name='change_bio'),
-    path('change-username/', views.change_username, name='change_username'),
-    path('change-password/', views.change_password, name='change_password'),
-    path('change-bio/', views.change_bio, name='change_bio'),
-    path('delete_profile_photo/', views.delete_profile_photo, name='delete_profile_photo'),
+    path('', views.userprofile, name='userprofile'),
+    path('settings/', views.settings, name='settings'),
 ]
